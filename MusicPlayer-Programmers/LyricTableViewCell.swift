e49d02287fbde48937ac97f5b501ee288d27c746
kdw <doowone8650@gmail.com>
2023-04-26 20:50:04 +0900

    LyricTableViewCell - Snapkit을 이용해 코드로 초기화

--- MusicPlayer-Programmers/LyricTableViewCell.swift
+++ MusicPlayer-Programmers/LyricTableViewCell.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import SnapKit
 
 class LyricTableViewCell: UITableViewCell {
+    // label 생성
 
-    @IBOutlet weak var lyricLabel: UILabel!
+    let lyricLabel = UILabel()
+    static let identifier = "lyricsTableViewCell"
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        lyricLabel.text = "가사정보"
+        lyricLabel.textColor = .black
+        lyricLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        lyricLabel.textAlignment = .left
+        
+        contentView.addSubview(lyricLabel)
+        
+        lyricLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview().offset(10)
+        }
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
